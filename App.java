255a083593e159c150b082a19eeb29aedf9ab0a6
mariammerza <mariammerza@trentu.ca>
2024-03-11 17:16:58 -0400

    Update App.java

--- App.java
+++ App.java
@@ -15,20 +15,44 @@ public class App extends JFrame {
     public App() {
         // JFrame settings
         //Setting Title
-        setTitle("Your Name's CSV Loader Application");
+        setTitle("Mariam's CSV Loader Application");
         //Setting Dimensions of Window
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        /*
-         * 1-Create a a new button named loadButton. The text in the Button should say LoadCSV
-         * 2-After that initialize a new TextArea (we already declared a textArea on Line 11), this is the area where we will display our CSV.
-         * 3-Set the new textAtrea to be uneditable (e.g., .setEditable(false))
-         * 4-Create a JScrollPane within the text area, so that we can scroll up and down.
-         * 5-Add an action listener to the button that calls the load CSV function (below)
-         * 6-Play around with coloring and styling to make your application look more professional
-         */
+
+         // 1-Create a a new button named loadButton. The text in the Button should say LoadCSV
+         JButton loadButton = new JButton("Load CSV");
+
+
+         // 2-After that initialize a new TextArea (we already declared a textArea on Line 11), this is the area where we will display our CSV.
+         textArea = new JTextArea();
+
+
+         // 3-Set the new textAtrea to be uneditable (e.g., .setEditable(false))
+         textArea.setEditable(false);
+
+
+         // 4-Create a JScrollPane within the text area, so that we can scroll up and down.
+         JScrollPane scrollPane = new JScrollPane(textArea);
+
+
+         // 5-Add an action listener to the button that calls the load CSV function (below)
+         loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //String filePath = "your_file_name.csv";
+                String filePath = "c:\\users\\marym\\OneDrive\\Documents\\Mariam.csv";
+                loadCsv(filePath);
+            }
+            } );
+
+
+         // 6-Play around with coloring and styling to make your application look more professional
+         loadButton.setBackground(Color.LIGHT_GRAY);
+         textArea.setBackground(Color.WHITE);
+         textArea.setForeground(Color.BLACK);
         
 
         add(loadButton, BorderLayout.NORTH);
